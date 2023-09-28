6a5b89c71702df439f3a91175fec617eeb5fc2ee
Dharan Aditya <dharan.aditya@outlook.com>
2023-09-28 17:39:26 +0530

    add lc algo

--- src/misc.rs
+++ src/misc.rs
@@ -1,4 +1,4 @@
-use std::cmp::max;
+use std::cmp::Ordering;
 
 fn vowel_count(s: &str) -> usize {
     let mut count = 0usize;
@@ -47,6 +47,26 @@ pub fn merge_alternately(word1: String, word2: String) -> String {
     m
 }
 
+pub fn reverse_vowels(s: String) -> String {
+    let is_vowel = |b: u8| matches!(b, b'a' | b'e' | b'o' | b'i' | b'u' | b'A' | b'E' | b'O' | b'I' | b'U');
+    let mut arr = s.into_bytes();
+    let (mut left, mut right) = (0, arr.len() - 1);
+    while left < right {
+        while left < right && !is_vowel(arr[left]) {
+            left += 1;
+        }
+        while left < right && !is_vowel(arr[right]) {
+            right -= 1;
+        }
+        if left < right {
+            arr.swap(left, right);
+            left += 1;
+            right -= 1;
+        }
+    }
+    String::from_utf8(arr).unwrap()
+}
+
 pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
     let mut res : Vec<bool> = Vec::new();
     let max = candies.iter().max().unwrap();
@@ -54,4 +74,50 @@ pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
         res.push(x + extra_candies >= *max);
     }
     return res;
+}
+
+
+pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+    let (mut left, mut right) = (0, numbers.len() - 1);
+    while left < right {
+        let sum = numbers[left] + numbers[right];
+        match sum.cmp(&target) {
+            Ordering::Equal => return vec![(left + 1) as i32, right + 1],
+            Ordering::Less => left += 1,
+            Ordering::Greater => right -= 1,
+        }
+    }
+    vec![]
+}
+
+pub fn combination_sum2(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+    let mut res: Vec<Vec<i32>> = Vec::new();
+    let mut buffer: Vec<i32> = Vec::new();
+    let mut _candidates = candidates.clone();
+    _candidates.sort_unstable();
+    solve(&_candidates, target, 0, &mut buffer, &mut res);
+    return res;
+}
+
+fn solve(arr: &[i32], target: i32, idx : usize, buffer: &mut Vec<i32>, res: &mut Vec<Vec<i32>>) {
+    let len = arr.len();
+    if idx == len || target == 0 {
+        if target == 0 {
+            res.push(buffer.clone());
+        }
+        return;
+    }
+    for i in idx..len {
+        if i > idx && arr[i] == arr[i-1] {
+            continue;
+        }
+        match target.cmp(&arr[i]) {
+            Ordering::Greater | Ordering::Equal => {
+                buffer.push(arr[i]);
+                solve(arr, target - arr[i], i + 1, buffer, res);
+                buffer.pop();
+            }
+            _ => ()
+        }
+    }
 }
